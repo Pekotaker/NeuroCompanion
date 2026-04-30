d11e8edeb2392f7c93d41ccb6bffc2c32e336c1e
pekotaker <loliisdabezt@gmail.com>
2026-05-01 00:04:58 +0700

    Polish Neuro Companion config menu

--- Configs/NeuroCompanionConfig.cs
+++ Configs/NeuroCompanionConfig.cs
@@ -7,39 +7,60 @@ namespace NeuroCompanion.Configs
     {
         public override ConfigScope Mode => ConfigScope.ClientSide;
 
+        [Header("Connection")]
         [DefaultValue("ws://localhost:8000")]
         public string RandyWebSocketUrl { get; set; }
 
         [DefaultValue(false)]
         public bool AutoConnectOnWorldLoad { get; set; }
 
+        [Header("Context")]
+        [Range(1, 60)]
+        [Slider]
         [DefaultValue(5)]
         public int PeriodicContextIntervalSeconds { get; set; }
 
         [DefaultValue(true)]
         public bool EnableEventContextMessages { get; set; }
 
+        [Range(1, 100)]
+        [Slider]
         [DefaultValue(35)]
         public int LowHealthPercent { get; set; }
 
+        [Header("ActionCooldowns")]
+        [Range(0, 30)]
+        [Slider]
         [DefaultValue(2)]
         public int RecallCooldownSeconds { get; set; }
 
+        [Range(0, 30)]
+        [Slider]
         [DefaultValue(1)]
         public int FollowCooldownSeconds { get; set; }
 
+        [Range(0, 30)]
+        [Slider]
         [DefaultValue(3)]
         public int AttackOnceCooldownSeconds { get; set; }
 
+        [Range(0, 60)]
+        [Slider]
         [DefaultValue(5)]
         public int AutoAttackCooldownSeconds { get; set; }
 
+        [Range(0, 300)]
+        [Slider]
         [DefaultValue(60)]
         public int BuffPlayerCooldownSeconds { get; set; }
 
+        [Range(0, 300)]
+        [Slider]
         [DefaultValue(60)]
         public int DebuffPlayerCooldownSeconds { get; set; }
 
+        [Range(0, 300)]
+        [Slider]
         [DefaultValue(30)]
         public int DebuffEnemyCooldownSeconds { get; set; }
     }
