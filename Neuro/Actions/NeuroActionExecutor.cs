58170c030aa73aea62817ef009df9f24e47c0052
pekotaker <loliisdabezt@gmail.com>
2026-07-02 21:41:53 +0700

    refactor: extract Neuro timed attack service

--- Neuro/Actions/NeuroActionExecutor.cs
+++ Neuro/Actions/NeuroActionExecutor.cs
@@ -1,20 +1,11 @@
-﻿using NeuroCompanion.Configs;
-using NeuroCompanion.Players;
+﻿using NeuroCompanion.Players;
 using System;
 using Terraria;
-using Terraria.ModLoader;
 
 namespace NeuroCompanion.Neuro
 {
     public static class NeuroActionExecutor
     {
-        private const int TicksPerSecond = 60;
-
-        private const int DefaultAttackDurationSeconds = 10;
-        private const int MinimumAttackDurationSeconds = 1;
-
-        private const float DebuffEnemySearchRange = 700f;
-
         public static NeuroActionResult Execute(
             Player player,
             NeuroCommand command
@@ -71,7 +62,10 @@ namespace NeuroCompanion.Neuro
                 case NeuroCommandType.StartTimedAttack:
                     return StartCooldownAndReturn(
                         command.Type,
-                        StartTimedAttack(neuroPlayer, command.DurationSeconds)
+                        NeuroTimedAttackService.StartTimedAttack(
+                            neuroPlayer,
+                            command.DurationSeconds
+                        )
                     );
 
                 case NeuroCommandType.AttackPlayer:
@@ -142,48 +136,6 @@ namespace NeuroCompanion.Neuro
             }
         }
 
-        private static NeuroActionResult StartTimedAttack(
-            NeuroCompanionPlayer neuroPlayer,
-            int requestedSeconds
-        )
-        {
-            int seconds = requestedSeconds;
-
-            if (seconds <= 0)
-            {
-                seconds = DefaultAttackDurationSeconds;
-            }
-
-            int maximumAttackDurationSeconds = GetMaximumAttackDurationSeconds();
-
-            seconds = Clamp(
-                seconds,
-                MinimumAttackDurationSeconds,
-                maximumAttackDurationSeconds
-            );
-
-            neuroPlayer.StartTimedAttack(seconds * TicksPerSecond);
-
-            return NeuroActionResult.Ok(
-                $"Neuro companion will attack for {seconds} seconds. Current maximum is {maximumAttackDurationSeconds} seconds."
-            );
-        }
-
-        private static int Clamp(int value, int minimum, int maximum)
-        {
-            if (value < minimum)
-            {
-                return minimum;
-            }
-
-            if (value > maximum)
-            {
-                return maximum;
-            }
-
-            return value;
-        }
-
         private static NeuroActionResult StartCooldownAndReturn(
             NeuroCommandType commandType,
             NeuroActionResult result
@@ -195,26 +147,6 @@ namespace NeuroCompanion.Neuro
             }
 
             return result;
-        }
-
-        private static int GetMaximumAttackDurationSeconds()
-        {
-            NeuroCompanionConfig config =
-                ModContent.GetInstance<NeuroCompanionConfig>();
-
-            int maximumSeconds = config.MaxAutoAttackDurationSeconds;
-
-            if (maximumSeconds < MinimumAttackDurationSeconds)
-            {
-                return MinimumAttackDurationSeconds;
-            }
-
-            if (maximumSeconds > 600)
-            {
-                return 600;
-            }
-
-            return maximumSeconds;
         }
     }
 }
---
+++ Neuro/Actions/NeuroTimedAttackService.cs
@@ -0,0 +1,72 @@
+﻿using NeuroCompanion.Configs;
+using NeuroCompanion.Players;
+using Terraria.ModLoader;
+
+namespace NeuroCompanion.Neuro
+{
+    public static class NeuroTimedAttackService
+    {
+        private const int TicksPerSecond = 60;
+
+        private const int DefaultAttackDurationSeconds = 10;
+        private const int MinimumAttackDurationSeconds = 1;
+        private const int HardMaximumAttackDurationSeconds = 600;
+
+        public static NeuroActionResult StartTimedAttack(
+            NeuroCompanionPlayer neuroPlayer,
+            int requestedSeconds
+        )
+        {
+            int seconds = requestedSeconds;
+
+            if (seconds <= 0)
+            {
+                seconds = DefaultAttackDurationSeconds;
+            }
+
+            int maximumAttackDurationSeconds =
+                GetMaximumAttackDurationSeconds();
+
+            seconds = Clamp(
+                seconds,
+                MinimumAttackDurationSeconds,
+                maximumAttackDurationSeconds
+            );
+
+            neuroPlayer.StartTimedAttack(seconds * TicksPerSecond);
+
+            return NeuroActionResult.Ok(
+                $"Neuro companion will attack for {seconds} seconds. Current maximum is {maximumAttackDurationSeconds} seconds."
+            );
+        }
+
+        private static int GetMaximumAttackDurationSeconds()
+        {
+            NeuroCompanionConfig config =
+                ModContent.GetInstance<NeuroCompanionConfig>();
+
+            int maximumSeconds = config.MaxAutoAttackDurationSeconds;
+
+            return Clamp(
+                maximumSeconds,
+                MinimumAttackDurationSeconds,
+                HardMaximumAttackDurationSeconds
+            );
+        }
+
+        private static int Clamp(int value, int minimum, int maximum)
+        {
+            if (value < minimum)
+            {
+                return minimum;
+            }
+
+            if (value > maximum)
+            {
+                return maximum;
+            }
+
+            return value;
+        }
+    }
+}
