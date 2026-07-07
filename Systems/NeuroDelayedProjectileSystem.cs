ca38bcf58d0fb96301b3b4805e47e9fe8998db5e
pekotaker <loliisdabezt@gmail.com>
2026-07-07 23:30:40 +0700

    TrySpawnWeaponProjectile

---
+++ Systems/NeuroDelayedProjectileSystem.cs
@@ -0,0 +1,14 @@
+﻿using Terraria.ModLoader;
+
+using NeuroCompanion.Projectiles.Helpers;
+
+namespace NeuroCompanion.Systems
+{
+    public class NeuroDelayedProjectileSystem : ModSystem
+    {
+        public override void PostUpdateProjectiles()
+        {
+            NeuroProjectileSpawner.UpdateDelayedWeaponShots();
+        }
+    }
+}
